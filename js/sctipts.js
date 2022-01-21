25a67e1197507c15e23a6eb499d8b92a1e74e8a0
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-21 09:01:11 -0800

    pass test 8 for beepBoop() function

--- js/sctipts.js
+++ js/sctipts.js
@@ -2,12 +2,12 @@
 
 function beepBoop(input) {
 	let number = parseInt(input);
-  if (number.toString().includes(1)) {
-  	number = "Beep!";
+  if (number.toString().includes(3)) {
+  	number = "Won't you be my neighbor?";
   } else if (number.toString().includes(2)) {
     number = "Boop!";
-  } else if (number.toString().includes(3)) {
-    number = "Won't you be my neighbor?";
+  } else if (number.toString().includes(1)) {
+    number = "Beep!";
   }
 
   return number;
