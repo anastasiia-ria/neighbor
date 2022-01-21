589e166162ac49648840c203016cd674016a528d
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-21 15:13:34 -0800

    clean scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -15,31 +15,36 @@ function beepBoop(input, name) {
       array.push(i)
     }
   }
-
   return array;
 }
 
 //UI Logic
+
 $(document).ready(function(){
   $('button#start').click(function (){
+    //Clear input
+    $('#number').val('');
+    $('#name').val('');
+    //Start form and clear previous results
     $('#cloud-form').show();
     $('button#start').hide();
     $('#result').hide();
-    $('#number').val('');
-    $('#name').val('');
+    $('#result').empty();
+    //Stop cloud animation
+    $('.cloud').removeClass('shake');
   });
 
   $('form#form').submit(function(event) {
     event.preventDefault();
-    console.log("Here");
-    $('#result').empty();
+    //Show results
     $('#cloud-form').hide();
     $('#result').show();
     $('button#start').show();
-    
+    $('.cloud').addClass('shake');
+    //Save input
     const number = $('#number').val();
     const name = $('#name').val();
-
+    
     beepBoop(number, name).forEach(function(number, index) {
       setTimeout(function(){
         $('ul#result').append('<li>' + number + '</li>');
