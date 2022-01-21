86fda40643ab9df1714d7fe1ac533fdd26b029d4
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-21 08:48:28 -0800

    pass test 3 for beepBoop() function

--- js/sctipts.js
+++ js/sctipts.js
@@ -4,7 +4,9 @@ function beepBoop(input) {
 	let number = parseInt(input);
   if (number === 1) {
   	number = "Beep!";
-  }
+  } else if (number === 2) {
+    number = "Boop!";
+  } 
 
   return number;
 }
