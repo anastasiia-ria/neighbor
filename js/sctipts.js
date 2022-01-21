a871bd5b36dd9b309ebacd5596ecf923111538f2
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-21 08:50:18 -0800

    pass test 4 for beepBoop() function

--- js/sctipts.js
+++ js/sctipts.js
@@ -6,7 +6,9 @@ function beepBoop(input) {
   	number = "Beep!";
   } else if (number === 2) {
     number = "Boop!";
-  } 
+  } else if (number === 3) {
+    number = "Won't you be my neighbor?";
+  }
 
   return number;
 }
